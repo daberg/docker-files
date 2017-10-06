253e2487100b6b6d8ef1fb3dd682f738863e7bfa
daberg <daberg@openmailbox.org>
2017-10-06 23:33:06 +0200

    First commit. Add nextcloud and reverse proxy.

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if ! [ $(id -u) = 0 ]; then
+   echo "This script should be run as root. Aborting..."
+   exit 1
+fi
+
+echo -n "Setting up configuration files..."
+
+# Setting up .env file for docker-compose configuration
+cp ./templates/general .env
+
+# Setting up environment variables files for services
+mkdir -p .servenv
+cp ./templates/*env -t ./.servenv
+
+PASSWD=`openssl rand -base64 24 | cut -c1-16 | sed 's/\//a/g'`
+
+echo "POSTGRES_PASSWORD=$PASSWD" >> ./.servenv/nc-env
+echo "POSTGRES_PASSWORD=$PASSWD" >> ./.servenv/nc-db-env
+
+echo " done."
+echo
+echo "Specific environment variables must be set in the .env file before\
+ running containers."
